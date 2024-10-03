451601b8984ffa52ad7a65462edcc51ac98c3b43
ancu-98 <jdsebastianb30@hotmail.com>
2024-10-02 23:11:39 -0500

    correcting auth router services

--- Backend/src/auth/auth.router.js
+++ Backend/src/auth/auth.router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const postLogin = require('./auth.services');
+const {postLogin} = require('./auth.services');
+const {postRecoveryToken} = require('./auth.services');
 
 router.post('/login', postLogin)
-router.post('/recovery-password', authServices.postRecoveryToken)
+router.post('/recovery-password', postRecoveryToken)
 
 module.exports = router
